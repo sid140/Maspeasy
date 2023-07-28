78a342cada8a3e6dd56118b300fce93050b6485b
Siddex <920976+sid140@users.noreply.github.com>
2023-07-29 01:03:14 +0530

    Add files via upload

--- mapeasy.py
+++ mapeasy.py
@@ -3,7 +3,7 @@
 import subprocess
 import argparse
 
-print("Welcome to Mapeasy, a simple dumbdown custom version of Nmap. Developed by Sid140")
+print("Welcome to Mapeasy, a simple dumbdown custom version of Nmap. Developed by Xalt\n Please run proxychains by typing proxynow command before using othe commands in the help section")
 try:
 	parser = argparse.ArgumentParser(description="nmap made easy")
 	parser.add_argument("--SYNSCAN", action="store_true", help="This is a TCP SYN SCAN, also known as a stealth scan.")
@@ -11,25 +11,37 @@ try:
 	parser.add_argument("--UDPSCAN", action="store_true", help="This scan is used to scan for UDP ports.")
 	parser.add_argument("--FASTPING", action="store_true", help="This is a simple and fast ping scan to see which hosts reply to ICMP ping packets.")
 	parser.add_argument("--VERSCAN", action="store_true", help="This is a service version scan. In order to know what exploits will work")
+	parser.add_argument("--proxynow", action="store_true", help="This will install proxychains and tor")
 	args = parser.parse_args()
 	user_input = input("Enter the IP address: ")
+	
 except ChildProcessError:
 	print("OPeration not working..Exiting")
 	raise e
 
+def proxynow():
+subprocess.run(["sudo", "apt-get", "-o", "Acquire::http::Proxy=http://localhost:9050", "update"], check=True)
+subprocess.run(["sudo", "apt-get", "-o", "Acquire::http::Proxy=http://localhost:9050", "install", "proxychains"], check=True)
+
 
 if args.SYNSCAN:
-    subprocess.run(["nmap", "-sS", user_input], check=True)
+    subprocess.run(["proxychains","nmap", "-sS", user_input], check=True)
 
 if args.TwSCAN:
-	subprocess.run(["nmap", "-sn", user_input], check=True)
+	subprocess.run(["proxychains","nmap", "-sn", user_input], check=True)
 	
 
 if args.UDPSCAN:
-	subprocess.run(["nmap", "-sU", user_input], check=True)
+	subprocess.run(["proxychains","nmap", "-sU", user_input], check=True)
 
 if args.FASTPING:
-	subprocess.run(["nmap", "-sn", user_input], check=True)
+	subprocess.run(["proxychains","nmap", "-sn", user_input], check=True)
 
 if args.VERSCAN:
-	subprocess.run(["nmap", "-sV", user_input], check=True)
+	subprocess.run(["proxychains","nmap", "-sV", user_input], check=True)
+
+if args.proxynow:
+	proxynow()
+
+
+
